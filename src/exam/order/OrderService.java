22cf6bac1e54677ea3262a520d8e117dfb8d33bf
blueberrylatte <yoffle0987@gmail.com>
2025-03-21 17:01:39 +0900

    마지막수정

--- src/exam/order/OrderService.java
+++ src/exam/order/OrderService.java
@@ -2,11 +2,12 @@ package exam.order;
 
 public class OrderService {
 
-		public void insert() {
-			System.out.println("bug/order");
+	public void insert() {
+		System.out.println("bug/order");
+	}
 
-public void select() {
-			System.out.println("원격 main 브랜치에서 작업함");
+	public void select() {
+		System.out.println("원격 main 브랜치에서 작업함");
 
-		}
+	}
 }
